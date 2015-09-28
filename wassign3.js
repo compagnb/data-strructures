6d84f704e1ad3a1dd4eafbe23770e2f0f1c730d9
Barbara Compagnoni <compagnb@newschool.edu>
2015-09-27 23:24:40 -0400

    weekly3

--- wassign3.js
+++ wassign3.js
@@ -28,16 +28,16 @@
 
 // // gather data from document and format it into google api
 // $('table[cellpadding=5]').find('tbody').find('tr').each (function (i, elem){
-//     meetings.push( $(elem).find('td').eq(0).html().split('<br>')[2].trim()); 
-    
+//     meetings.push( $(elem).find('td').eq(0).html().split('<br>')[2].trim());
+
 //     cleanMeetings.push(meetings[i].substring(0, meetings[i].indexOf(',')));
 //     fullAddress[i] = cleanMeetings[i] + ', New York, NY';
 //     googleAddy[i] = (fullAddress[i].split(' ').join('+'));
-    
+
 //     // for testing
 //     // console.log(googleAddy[i]);
 //     });
-    
+
 // fs.writeFileSync('/home/ubuntu/workspace/data/addresses.txt', JSON.stringify(googleAddy));
 
 
@@ -47,7 +47,7 @@ var async = require('async'); // npm install async
 var request = require('request'); // npm install request
 
 // for google API
-// SETTING ENVIRONMENT VARIABLES (in Linux): 
+// SETTING ENVIRONMENT VARIABLES (in Linux):
 // export NEW_VAR="Content of NEW_VAR variable"
 // printenv | grep NEW_VAR
 var apiKey ='AIzaSyA-CKC1h7HYmCnIminO6aSpD0yaAxNTXw4';
@@ -60,24 +60,24 @@ var addys = JSON.parse(fs.readFileSync('/home/ubuntu/workspace/data/addresses.tx
 
 
 // eachSeries in the async module iterates over an array and operates on each item in the array in series
-// // calls for array and the function ot ogo over 
+// // calls for array and the function ot ogo over
 async.eachSeries(addys, function(value, callback) {
-    
+
     // make an api request using the addy pulled in and the api key
     var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + value + '&key=' + apiKey;
-    
+
     // for testing purposes
     // console.log (apiRequest);
-    
+
     var thisMeeting = new Object;
     thisMeeting.address = value;
-    
+
     request(apiRequest, function(err, resp, body) {
         if (err) {
             throw err;
         }
-        
-        if (JSON.parse(body).status == "ZERO_RESULTS") { 
+
+        if (JSON.parse(body).status == "ZERO_RESULTS") {
             console.log("ZERO RESULTS for" + value);
         } else {
             thisMeeting.latLong = JSON.parse(body).results[0].geometry.location;
@@ -87,9 +87,9 @@ async.eachSeries(addys, function(value, callback) {
     setTimeout(callback, 300);
 }, function() {
      fs.writeFile('/home/ubuntu/workspace/data/wassign3.txt', JSON.stringify(meetingInfo), function (err) {
-         if (err) 
+         if (err)
          return console.log('Error');
-         console.log('Wrote ' + meetingInfo.length + ' entries to file ' + 'wassign3.txt');
-        
+         console.log('completed');
+
      });
 });
