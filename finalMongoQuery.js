dad2018fa813d0fd65b3bef040c531d4bb468975
compagnb <compagnb@newschool.edu>
2015-12-04 16:55:14 +0000

    Final Query for AA

--- finalMongoQuery.js
+++ finalMongoQuery.js
@@ -32,9 +32,9 @@ var endDayNum = getEndDay(startDayNum);
 
 // function to change the number by adding one, unless its a 7 then reset to 0
 function getEndDay(startDayNum){
-    if (startDayNum < 7){
+    if (startDayNum != 7){
         return startDayNum+1;
-    } else {
+    } else if (startDayNum == 7){
         return 0
     }
     // test console
@@ -59,38 +59,60 @@ MongoClient.connect(url, function(err, db) {
         
         $or: [{
         
-            $and: [{ meetingDay: startDayNum },
+            $and: [{ meetingDayNum: startDayNum },
             { meetingStartHr: { $gt: currentTime, $lt: 25 } } 
             ]},
         
-            { $and: [{ meetingDay: endDayNum },
+            { $and: [{ meetingDayNum: endDayNum },
             { meetingStartHr: { $gt: -1, $lt: endTime } } 
             ]}
     ]}},
     
     // group results by address (maybe change to lat long)
-    {$group: {_id : "$cleanedAddress",
-        meetingName: {$push: "$meetingName"}, 
-        locationName: {$push: "$locationName"},
-        addressMoreInfo: {$push: "$addressMoreInfo"},
-        zipcode: {$push: "$zipcode"},
-        lat: {$push: "$latLong.lat"},
-        long: {$push: "$latLong.long"},
-        meetingType: {$push: "$meetingType"},
-        specialInterest: {$push: "$SpecialInterest"},
-        handiAccess: {$push: "$handiAccess"},
-        specialInfo: {$push: "$specialInfo"},
-        meetingDay: {$push: "$meetingDay"},
-        meetingStartTime: {$push: "$meetingStartTime"},
-        meetingStartHr: {$push: "$meetingStartHr"},
-        meetingStartMin: {$push: "$meetingStartMin"}
-    } 
+    { $group : { _id : { 
+        meetingName: "$meetingName", 
+        locationName: "$locationName",
+        meetingAddress: "$cleanedAddress",
+        addressMoreInfo: "$addressMoreInfo",
+        zipcode: "$zipcode",
+        specialInfo: "$specialInfo",
+        handiAccess: "$handiAccess",
+        latLong: "$latLong"
+        },
+    
+        meetingDay: { $push :"$meetingDay"},
+        meetingStartTime: { $push : "$meetingStartTime"},
+        meetingStartHr: { $push : "$meetingStartHr"},
+        meetingStartMin: { $push : "$meetingStartMin"},
+        meetingEndTime: { $push : "$meetingEndTime"},
+        meetingType: { $push : "$meetingType"},
+        specialInterest: { $push : "$SpecialInterest"}
+        }},
         
-    }]).toArray(function(err, docs) {
+    { $group : { _id : { latLong : "$_id.latLong" }, 
+        meetingGroups: {$addToSet: { meetingGroup: "$_id", 
+                                meetings : {
+                                meetingDays : "$meetingDay",
+                                startTimes : "$meetingStartTime",
+                                startTimeHours : "$meetingStartHr",
+                                endTimes : "$meetingEndTime",
+                                meetingTypes : "$meetingType",
+                                specialInterest : "$SpecialInterest"
+                                }
+        } }
+    } }
+    
+        
+    ]).toArray(function(err, docs) {
         if (err) {console.log(err)}
         
         else {
-            console.log(docs);
+            //console.log(docs);
+            // console.log(JSON.stringify(docs));
+            for (var i=0; i < docs.length; i++) {
+                console.log(JSON.stringify(docs[i], null, 4));
+                console.log('');
+            }
         }
         db.close();
         
